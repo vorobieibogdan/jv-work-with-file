9a65d3df9fe2cb2fd89a5939cad96fedd4ab6d51
Bohdan Vorobiei <vbs@ukr.net>
2025-11-08 16:48:27 +0200

    The task is probably complete.5

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,11 +5,13 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
     private static final String SEPARATOR = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
         String report = createReport(readFile(fromFileName));
         writeToFile(toFileName, report);
